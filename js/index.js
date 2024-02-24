2c08382457af721101995fae3c075fc5335b9c59
JonasRiek <159618255+JonasRiek@users.noreply.github.com>
2024-02-24 08:27:11 +0100

    Test 2: Commit wrong Code in JS to Main

--- js/index.js
+++ js/index.js
@@ -29,15 +29,15 @@ strictButton.addEventListener('click', (event) => {
 
 onButton.addEventListener('click', (event) => {
   if (onButton.checked == true) {
-    on = true
-    turnCounter.innerHTML = '-'
+    on = true;
+    turnCounter.innerHTML = '-';
   } else {
     on = false;
     turnCounter.innerHTML = '';
     clearColor();
     clearInterval(intervalId);
   }
-});
+
 
 startButton.addEventListener('click', (event) => {
   if (on || win) {
